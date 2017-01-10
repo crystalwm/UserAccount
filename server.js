bebcbec468e5889561446aeab6fefb699538b135
crystalwm <1207356200@qq.com>
2017-01-10 19:29:16 +0800

    commit

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-  const webpack          = require('webpack');
+  const webpack = require('webpack');
   const WebpackDevServer = require("webpack-dev-server");
-  const config           = require('./webpack.config.babel');
+  const config = require('./webpack.config.babel');
 
-const compiler = webpack(config);
+  const compiler = webpack(config);
 
-  var server = new WebpackDevServer(compiler, {
-    hot: true,
-    quiet: true,
-    noInfo: false,
-    stats: {colors: true},
-    historyApiFallback: true,
-    clientLogLevel: "warning",
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
-    }
+  compiler.run((err, stats) => {
+      if (err) {
+          console.log("webpack compiler err:\n");
+          console.log(err);
+      }
+
+      console.log("webpack the project ");
   });
 
+
+  /*
+    var server = new WebpackDevServer(compiler, {
+        hot: true,
+        quiet: true,
+        noInfo: false,
+        stats: { colors: true },
+        historyApiFallback: true,
+        clientLogLevel: "warning",
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000
+        }
+    });
+
     // Webpack dev server
-  server.listen(4005)
+    server.listen(4005)
+    */
--- test.js
+++
@@ -1,5 +0,0 @@
-import path from 'path'
-
-var a = path.resolve(__dirname, 'assets', 'styles', 'styles.css');
-
-console.log(a);
